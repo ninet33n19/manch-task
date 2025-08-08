104f18303af8a7e6aac2381e91c73c72700b668c
ninet33n19 <vaidyameet2004@gmail.com>
2025-08-08 23:01:02 +0530

    Add custom Garamond and Bergen fonts and update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 export const theme = {
   extend: {
     fontFamily: {
-      garamond: ['"EB Garamond"'],
-      josefin: ['"Josefin Sans"'],
-      sans: ['"Josefin Sans"', 'sans-serif'],
+      garamond: ["Garamond", "serif"],
+      bergen: ["Bergen", "sans-serif"],
     },
   },
 };
